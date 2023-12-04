ab4fbeae38182be1565093f2751ee8ea0c0d59d6
Kevin Guthrie <kbg@amazon.com>
2023-12-04 10:25:47 -0500

    Adding Day 4 and a new helper function for parsing a contents of a string surrounded by white space

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,19 @@ mod day;
 pub mod template;
 
 pub use day::*;
+
+use nom::{
+    character::complete::multispace0, error::ParseError, sequence::delimited,
+    IResult, Parser,
+};
+
+/// A combinator that takes a parser `inner` and produces a parser that also consumes both leading and
+/// trailing whitespace, returning the output of `inner`.
+pub fn ws<'a, F, O, E: ParseError<&'a str>>(
+    inner: F,
+) -> impl Parser<&'a str, O, E>
+where
+    F: Parser<&'a str, O, E>,
+{
+    delimited(multispace0, inner, multispace0)
+}
